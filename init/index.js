e29768f5751715e004e57ecf204e24a7d8292b6a
Yuval-Varke <varke.vishu01@gmail.com>
2025-10-30 17:46:06 +0530

    Updated Sec Concerns

--- init/index.js
+++ init/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb+srv://yuval:yuval123@cluster0.fjf8vzg.mongodb.net/"
+const MONGO_URL = process.env.MONGO_URL;
 
 main()
   .then(() => {
